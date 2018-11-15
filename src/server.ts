dcb3b9610d3306c0f96e27d126efecc42b1ae033
Mike Solomon <mike@mikesolomon.org>
2018-11-15 21:35:03 +0200

    Adds now

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-import express from 'express';
+import debug from "debug";
+import express from "express";
+import _ from "lodash";
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req: express.Request, res: express.Response) => {
+    const feedback = [
+        "Thanks for the free credentials. We just used them to hack your production environment.",
+        "Your unconditional optimism about the future is inspiring.",
+        "This joke is unfunny.",
+        "You needed for this to happen to you so that you could hit rock bottom. Don't worry.",
+        "You deserve better than this.",
+        "Testing is overrated.",
+    ];
+    res.send(_.sample(feedback));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = process.env.PORT || 3000;
+app.listen(3000, () => debug("i:forgot")(`listening on port ${port}!`));
